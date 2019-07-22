1301426c86704201c7a880e33605430ee6050648
Julie Thrall <juliethrall10@gmail.com>
2019-07-22 16:17:01 -0700

    in progress sanity save

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,29 @@
 import React, { useState } from 'react';
-
+import { Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList.js';
+import Movie from './Movies/Movie.js';
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+	const [
+		savedList,
+		setSavedList
+	] = useState([]);
 
-  const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
-  };
+	const addToSavedList = (movie) => {
+		setSavedList([
+			...savedList,
+			movie
+		]);
+	};
 
-  return (
-    <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
-    </div>
-  );
+	return (
+		<div>
+			<SavedList list={savedList} />
+			<Route path="/" component={MovieList} />
+			<Route exact path="/movies/ :id" component={Movie} />
+		</div>
+	);
 };
 
 export default App;
