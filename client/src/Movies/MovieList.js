abedfe9bbdb64f7c8f369e816de4f8f39a180522
Julie Thrall <juliethrall10@gmail.com>
2019-07-23 18:24:06 -0700

    completed stretch using solution code for ref

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
 const MovieList = (props) => {
 	const [
@@ -22,40 +22,15 @@ const MovieList = (props) => {
 		getMovies();
 	}, []);
 
-	console.log(movies);
-
 	return (
 		<div className="movie-list">
-			{movies.map((movie) => (
-				<Link to={`/movies/${movie.id}`}>
-					<MovieDetails key={movie.id} movie={movie} />
-					{/* <MovieDetails key={movie.id} movie={movie} /> */}
-				</Link>
-			))}
+			{movies.map((movie) => <MovieDetails key={movie.id} movie={movie} addToSavedList={props.addToSavedList} />)}
 		</div>
 	);
 };
 
-function MovieDetails ({ movie }) {
-	const { title, director, metascore, stars } = movie;
-	return (
-		<div className="movie-card">
-			<h2>{title}</h2>
-			<div className="movie-director">
-				Director: <em>{director}</em>
-			</div>
-			<div className="movie-metascore">
-				Metascore: <strong>{metascore}</strong>
-			</div>
-			<h3>Actors</h3>
-
-			{stars.map((star) => (
-				<div key={star} className="movie-star">
-					{star}
-				</div>
-			))}
-		</div>
-	);
+function MovieDetails (props) {
+	return <MovieCard movie={props.movie} addToSavedList={props.addToSavedList} />;
 }
 
 export default MovieList;
